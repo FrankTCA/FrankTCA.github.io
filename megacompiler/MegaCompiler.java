b63dddae0c6df5e301a26842b984554d1a412b89
FrankTCA <frank@steamedveggie.com>
2016-08-17 12:32:33 -0500

    DeleteMegaCompilerSrc6

--- megacompiler/MegaCompiler.java
+++
@@ -1,9 +0,0 @@
-package megacompiler;
-
-public class MegaCompiler {
-    public static void main(String[] args) {
-        System.out.println("Starting MegaCompiler");
-        MainGUI gui = new MainGUI();
-        gui.setVisible(true);
-    }
-}
