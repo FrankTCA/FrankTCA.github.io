a9b7bb6b2b354f2b23b978751cbd00582e374471
FrankTCA <frank@steamedveggie.com>
2016-08-17 12:13:36 -0500

    MegaCompilerSrc

---
+++ megacompiler/MegaCompiler.java
@@ -0,0 +1,9 @@
+package megacompiler;
+
+public class MegaCompiler {
+    public static void main(String[] args) {
+        System.out.println("Starting MegaCompiler");
+        MainGUI gui = new MainGUI();
+        gui.setVisible(true);
+    }
+}
