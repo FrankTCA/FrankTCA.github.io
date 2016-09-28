51a51b1e1fa806b97cf2828eae5e2d84f0efdf07
FrankTCA <frank@steamedveggie.com>
2016-09-28 11:25:24 -0500

    AfricanReligions Add Code

---
+++ AfricanReligions/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $('.invisibleC').hide();
+  $('.invisibleI').hide();
+  $('#descriptionAreaC').hide();
+  $('#n1').hover(function() {
+    $('.invisibleC').fadeIn(false);
+  },
+                 function() {
+                   $('.invisibleC').fadeOut(false);
+                 });
+  $('#n1').click(function() {
+    $('#descriptionAreaC').slideToggle(200);
+  });
+  $('#n2').hover(function() {
+    $('.invisibleI').fadeIn(false);
+  },
+                 function() {
+                   $('.invisibleI').fadeOut(false);
+                 });
+  
+});
